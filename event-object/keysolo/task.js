4195fc6f48b3c4d51b22637e4feeeb078d1f48c7
Леоненко Дмитрий <dimaleonenko34@gmail.com>
2022-05-31 21:42:57 +1000

    homework2

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,7 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    
 	 document.addEventListener('keyup', (event) => {
 		 let requiredSymbol = this.currentSymbol.textContent.toLowerCase();
 		 let inputSymbol = event.key.toLowerCase();
