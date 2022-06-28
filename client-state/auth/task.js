df38f5e775b10a6c7b28ac7636c07ba7ba9f62c5
Леоненко Дмитрий <dimaleonenko34@gmail.com>
2022-06-28 20:04:03 +1000

    homework3

---
+++ client-state/auth/task.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('signin__form');
+const userID = document.getElementById('user_id');
+const welcomeModal = document.getElementById('welcome');
+
+function welcomeUser() {
+	form.classList.add('welcome');
+	welcomeModal.classList.add('welcome_active');
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+
+	xhr.responseType = 'json';
+
+	const formData = new FormData(form);
+	xhr.send(formData);
+
+	xhr.onload = function() {
+		let responseObj = xhr.response;
+		// console.log(responseObj.user_id);
+
+		if(responseObj.user_id) {
+			userID.textContent = responseObj.user_id;
+			
+			welcomeUser();
+
+			localStorage.setItem('id', userID.textContent);
+			// console.log(localStorage);
+		} else {
+			alert('Неверный логин/пароль');
+		}
+	}
+
+	form.reset();
+});
+
+window.onload = function() {
+	const userId = localStorage.getItem('id');
+	console.log(userId);
+
+	if(userId) {
+		userID.textContent = userId;
+		welcomeUser();
+	}
+}
